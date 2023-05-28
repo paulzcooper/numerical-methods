245a82f202b5c2ecf770cc04dadbf8e7f57e675a
Polina Artiukh <polina.zemko@rallyware.com>
2023-05-28 16:18:54 +0200

    fix comments

--- runge_kutta.py
+++ runge_kutta.py
@@ -3,11 +3,6 @@ import os
 import numpy as np
 from scipy.optimize import fsolve
 
-EXPORT_DIR_PATH = 'data/'
-
-if not os.path.exists(EXPORT_DIR_PATH):
-    os.makedirs(EXPORT_DIR_PATH)
-
 
 def ksystem(k_coeffs: np.array, *hxyz: tuple) -> list:
     """
@@ -22,37 +17,37 @@ def ksystem(k_coeffs: np.array, *hxyz: tuple) -> list:
     :param xyz:
     :return:
     """
-    system, h, xyz = hxyz[0], hxyz[1], hxyz[2:]
+    vector_f, h, xyz = hxyz[0], hxyz[1], hxyz[2:]
     print("k_coeffs = ", k_coeffs)
     coeff_system = [  # equation for k_coeffs[0] k1x
-        k_coeffs[0] - h * system[0](xyz[0] + k_coeffs[0] / 4 + (1 / 4 - np.sqrt(3) / 6) * k_coeffs[1],
+        k_coeffs[0] - h * vector_f[0](xyz[0] + k_coeffs[0] / 4 + (1 / 4 - np.sqrt(3) / 6) * k_coeffs[1],
                                     xyz[1] + k_coeffs[2] / 4 + (1 / 4 - np.sqrt(3) / 6) * k_coeffs[3],
                                     xyz[2] + k_coeffs[4] / 4 + (1 / 4 - np.sqrt(3) / 6) * k_coeffs[5]),
         # equation for k_coeffs[2] k1y
-        k_coeffs[2] - h * system[1](xyz[0] + k_coeffs[0] / 4 + (1 / 4 - np.sqrt(3) / 6) * k_coeffs[1],
+        k_coeffs[2] - h * vector_f[1](xyz[0] + k_coeffs[0] / 4 + (1 / 4 - np.sqrt(3) / 6) * k_coeffs[1],
                                     xyz[1] + k_coeffs[2] / 4 + (1 / 4 - np.sqrt(3) / 6) * k_coeffs[3],
                                     xyz[2] + k_coeffs[4] / 4 + (1 / 4 - np.sqrt(3) / 6) * k_coeffs[5]),
         # equation for k_coeffs[4] k1z
-        k_coeffs[4] - h * system[2](xyz[0] + k_coeffs[0] / 4 + (1 / 4 - np.sqrt(3) / 6) * k_coeffs[1],
+        k_coeffs[4] - h * vector_f[2](xyz[0] + k_coeffs[0] / 4 + (1 / 4 - np.sqrt(3) / 6) * k_coeffs[1],
                                     xyz[1] + k_coeffs[2] / 4 + (1 / 4 - np.sqrt(3) / 6) * k_coeffs[3],
                                     xyz[2] + k_coeffs[4] / 4 + (1 / 4 - np.sqrt(3) / 6) * k_coeffs[5]),
         # equation for k_coeffs[1] k2x
-        k_coeffs[1] - h * system[0](xyz[0] + (1 / 4 + np.sqrt(3) / 6) * k_coeffs[0] + k_coeffs[1] / 4,
+        k_coeffs[1] - h * vector_f[0](xyz[0] + (1 / 4 + np.sqrt(3) / 6) * k_coeffs[0] + k_coeffs[1] / 4,
                                     xyz[1] + (1 / 4 + np.sqrt(3) / 6) * k_coeffs[2] + k_coeffs[3] / 4,
                                     xyz[2] + (1 / 4 + np.sqrt(3) / 6) * k_coeffs[4] + k_coeffs[5] / 4),
         # equation for k_coeffs[3] k2y
-        k_coeffs[3] - h * system[1](xyz[0] + (1 / 4 + np.sqrt(3) / 6) * k_coeffs[0] + k_coeffs[1] / 4,
+        k_coeffs[3] - h * vector_f[1](xyz[0] + (1 / 4 + np.sqrt(3) / 6) * k_coeffs[0] + k_coeffs[1] / 4,
                                     xyz[1] + (1 / 4 + np.sqrt(3) / 6) * k_coeffs[2] + k_coeffs[3] / 4,
                                     xyz[2] + (1 / 4 + np.sqrt(3) / 6) * k_coeffs[4] + k_coeffs[5] / 4),
         # equation for k_coeffs[5] k2z
-        k_coeffs[5] - h * system[2](xyz[0] + (1 / 4 + np.sqrt(3) / 6) * k_coeffs[0] + k_coeffs[1] / 4,
+        k_coeffs[5] - h * vector_f[2](xyz[0] + (1 / 4 + np.sqrt(3) / 6) * k_coeffs[0] + k_coeffs[1] / 4,
                                     xyz[1] + (1 / 4 + np.sqrt(3) / 6) * k_coeffs[2] + k_coeffs[3] / 4,
                                     xyz[2] + (1 / 4 + np.sqrt(3) / 6) * k_coeffs[4] + k_coeffs[5] / 4),
     ]
     return coeff_system
 
 
-def solve_ksystem(system_to_solve: callable, xyz: tuple, h: float,
+def solve_ksystem(system_to_solve: callable, xyz: tuple, h: float, vector_f: list,
                   k0: np.array = np.array((0, 0, 0, 0, 0, 0))) -> np.array:
     """
 
@@ -61,18 +56,16 @@ def solve_ksystem(system_to_solve: callable, xyz: tuple, h: float,
     :param hxyz: initial conditions for the system
     :return: list of roots (k_coeffs)
     """
-    hxyz = (h, *xyz)
-    args = (vallis_system, h, *xyz)
-    print("hxyz = ", hxyz, "k0 = ", k0)
+    args = (vector_f, h, *xyz)
     root = fsolve(system_to_solve, k0, args=args)
     return root
 
 
-def make_one_step(xyz, h):
+def make_one_step(xyz, h, vector_f):
     """"
     Makes one step with the step size = h
     """
-    k_coeffs = solve_ksystem(system_to_solve=ksystem, xyz=xyz, h=h)
+    k_coeffs = solve_ksystem(system_to_solve=ksystem, xyz=xyz, h=h, vector_f=vector_f)
     xyz_one_step = np.array([xyz[0] + (k_coeffs[0] + k_coeffs[2]) / 2,
                              xyz[1] + (k_coeffs[2] + k_coeffs[3]) / 2,
                              xyz[2] + (k_coeffs[4] + k_coeffs[5]) / 2
@@ -80,13 +73,13 @@ def make_one_step(xyz, h):
     return xyz_one_step
 
 
-def make_two_steps(xyz: tuple, h: float):
+def make_two_steps(xyz: tuple, h: float, vector_f):
     """"
     Makes two steps with the step size = h/2
     """
     # k system coeffs (roots) for step = h/2
     h = h / 2
-    k_coeffs = solve_ksystem(system_to_solve=ksystem, xyz=xyz, h=h)
+    k_coeffs = solve_ksystem(system_to_solve=ksystem, xyz=xyz, h=h, vector_f=vector_f)
 
     # values for xyz with one step = h/2
     xyz_one_half_step = np.array([xyz[0] + (k_coeffs[0] + k_coeffs[2]) / 2,
@@ -113,28 +106,13 @@ def calculate_iter_errors(xyz_one_step: np.array, xyz_two_half_step: np.array) -
     return iter_errors
 
 
-def euclidian_norm(arr: np.array):
-    """"
-    Calculates Euclidian norm of errors vector: √(err_x^2 + err_y^2 + err_z^2)
-    """
-    eu_norm = np.sqrt(np.sum(np.power(arr, 2)))
-    return eu_norm
-
-
-def make_iteration():
-    pass
-
-
-def run(t_start, t_end, xyz0, h0: float = 0.1, epsilon: int = 10e-4):
+def run(t_start, t_end, xyz0, vector_f: list, h0: float = 0.1, epsilon: float = 10e-4, max_iter: int = 10_000):
     xyz = np.ndarray(shape=(1, 3))
     xyz[0] = xyz0
     tn = np.array([t_start])
 
     # arrays for component errors
     errors = np.array([0, 0, 0])
-    err_x = np.array([0])
-    err_y = np.array([0])
-    err_z = np.array([0])
 
     # array for errors norm
     error_norms = np.array([0])
@@ -145,7 +123,7 @@ def run(t_start, t_end, xyz0, h0: float = 0.1, epsilon: int = 10e-4):
 
     curr_iter = 0
     while tn[curr_iter] <= t_end:
-        if curr_iter > 10000:
+        if curr_iter > max_iter:
             break
 
         if tn[curr_iter] + steps[curr_iter] > t_end:
@@ -158,14 +136,15 @@ def run(t_start, t_end, xyz0, h0: float = 0.1, epsilon: int = 10e-4):
         print("h = ", h)
         print("xyz = ", xyz[curr_iter])
 
-        xyz_one_step = make_one_step(xyz=xyz[curr_iter], h=h)
+        xyz_one_step = make_one_step(xyz=xyz[curr_iter], h=h, vector_f=vector_f)
 
-        xyz_two_half_step = make_two_steps(xyz=xyz[curr_iter], h=h)
+        xyz_two_half_step = make_two_steps(xyz=xyz[curr_iter], h=h, vector_f=vector_f)
         print("xyz_two_half_step = ", xyz_two_half_step)
 
+        # Calculates Euclidian norm of errors vector: √(err_x^2 + err_y^2 + err_z^2)
         iter_errors = calculate_iter_errors(xyz_one_step=xyz_one_step, xyz_two_half_step=xyz_two_half_step)
 
-        iter_error_norm = euclidian_norm(arr=iter_errors)
+        iter_error_norm = np.linalg.norm(iter_errors)
         print(f"error norm for iter {curr_iter} = ", iter_error_norm)
         if iter_error_norm > epsilon:
 
@@ -178,10 +157,8 @@ def run(t_start, t_end, xyz0, h0: float = 0.1, epsilon: int = 10e-4):
             tn = np.append(tn, tn[curr_iter] + h)
             steps = np.append(steps, h)
 
+            errors = np.append(errors, iter_errors)
             error_norms = np.append(error_norms, iter_error_norm)
-            err_x = np.append(err_x, iter_errors[0])
-            err_y = np.append(err_y, iter_errors[1])
-            err_z = np.append(err_z, iter_errors[2])
 
             h = h * 2
             curr_iter += 1
@@ -192,10 +169,8 @@ def run(t_start, t_end, xyz0, h0: float = 0.1, epsilon: int = 10e-4):
             tn = np.append(tn, tn[curr_iter] + h)
             steps = np.append(steps, h)
 
+            errors = np.append(errors, iter_errors)
             error_norms = np.append(error_norms, iter_error_norm)
-            err_x = np.append(err_x, iter_errors[0])
-            err_y = np.append(err_y, iter_errors[1])
-            err_z = np.append(err_z, iter_errors[2])
 
             curr_iter += 1
 
@@ -207,18 +182,10 @@ def run(t_start, t_end, xyz0, h0: float = 0.1, epsilon: int = 10e-4):
 
 
 if __name__ == '__main__':
-    # def func_x(x, y, z):
-    #     mu = 80
-    #     a = 5
-    #     return mu * y - a * x
-    #
-    #
-    # def func_y(x, y, z):
-    #     return x * z - y
-    #
-    #
-    # def func_z(x, y, z):
-    #     return 1 - x * y - z
+    EXPORT_DIR_PATH = 'data/'
+
+    if not os.path.exists(EXPORT_DIR_PATH):
+        os.makedirs(EXPORT_DIR_PATH)
 
     mu = 80
     a = 5
@@ -227,22 +194,12 @@ if __name__ == '__main__':
     func_z = lambda x, y, z: 1 - x * y - z
     vallis_system = [func_x, func_y, func_z]
 
-    h = 0.1
-    xyz = (1, 1, 1)
-    k0_coeffs = np.array((0, 0, 0, 0, 0, 0))
-    hxyz = (h, *xyz)
-    s = ksystem(k0_coeffs, vallis_system, *hxyz)
-    print(s)
-
-    ss = solve_ksystem(system_to_solve=ksystem, xyz=xyz, h=h)
-    print(ss)
-
     # initial conditions
     t_start = 0
     t_end = 100
     h0 = 0.1
     xyz0 = (1, 1, 1)
 
-    tn, xyz_arr, err_norms = run(t_start=t_start, t_end=t_end, xyz0=xyz0, h0=h0)
+    tn, xyz_arr, err_norms = run(t_start=t_start, t_end=t_end, xyz0=xyz0, h0=h0, vector_f=vallis_system)
     np.savetxt(f'{EXPORT_DIR_PATH}tn.csv', tn)
     np.savetxt(f'{EXPORT_DIR_PATH}xyzn.csv', xyz_arr, delimiter=';')
